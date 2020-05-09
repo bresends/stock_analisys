1bef8b4f42bc56f22428f973591599567cd7bb20
Bruno <bruno.resendesantos45@gmail.com>
2020-05-09 18:40:45 -0300

    Duplicate Removal

--- src/stock_analisys/bastter_rating.py
+++ src/stock_analisys/bastter_rating.py
@@ -6,26 +6,4 @@ import pandas as pd
 
 # Defining Function to show company based on ticker
 
-
-class BastterRating:
-
-    def __init__(self, ticker):
-        self.ticker = ticker.upper()
-
-    def company_show(self):
-        """
-        Pick one company and shows its information
-
-        """
-
-        # Open dataframe with all info
-        self.df = pd.read_csv(
-            'data/bastter_analysis/bastter_stocks_analised.csv')
-
-        # Select one company from all in the DF
-        self.company_info = self.df[self.df['Ticker'] == self.ticker]
-
-
-stock = BastterRating('aapl')
-stock.company_show()
-
+df = pd.read_csv('data/bastter_analysis/bastter_stocks_analised.csv')
